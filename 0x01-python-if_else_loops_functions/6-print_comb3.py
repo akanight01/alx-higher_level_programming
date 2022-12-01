624140959374353dcd3a49522bb39474fddb77d9
AKEEM OLAYINKA AKANNI <100510532+acanite@users.noreply.github.com>
2022-12-01 08:54:00 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i != 99:
-        print("{:02d}, ".format(i), end="")
-    else:
-        print("{:02d}".format(i))
+count = 1
+for w in range(0, 9):
+    for i in range(count, 10):
+        if w == 8 and i == 9:
+            print("{}".format(89))
+            continue
+        print("{}{}, ".format(w, i), end="")
+    count = count + 1
