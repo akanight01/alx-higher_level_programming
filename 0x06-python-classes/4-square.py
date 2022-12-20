dae8c041f72ab6fdc54a3fa69b17abe1dae95b5f
AKEEM OLAYINKA AKANNI <100510532+acanite@users.noreply.github.com>
2022-12-20 14:47:45 +0100

    my commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,33 +1,31 @@
-#!/user/nin/python3
-""" square module
-"""
+#!/usr/bin/python3
+
+"""Define a class Square."""
 
 
 class Square:
-    """ Define a Square class
-    """
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """ Intialize methonds that store the size of the square. """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
 
     @property
     def size(self):
-        """ property for method size """
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """ Setter for method size """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        """ The methode area returns area of square """
-        return int(self.__size) * int(self.__size)
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
