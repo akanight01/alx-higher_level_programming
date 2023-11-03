3e53babc7600e0479176d1b59bcf8142030d9b4f
akanight01 <134161622+akanight01@users.noreply.github.com>
2023-11-03 11:55:27 +0100

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-safe_print_list = __import__('0-safe_print_list').safe_print_list
+# 0-square.py by Ehoneah Obed
+"""Defines a square """
 
-my_list = [1, 2, 3, 4, 5]
 
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+class Square:
+    """Does nothing"""
+    pass
